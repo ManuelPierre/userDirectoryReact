94ab91ccaf43f5220c26f423866799ada1133b69
TJ <tahmeenaowais@yahoo.com>
2020-11-05 18:58:22 -0500

    bootstrap fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, {
+  Component
+} from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
@@ -10,19 +12,60 @@ class App extends Component {
     friends
   };
 
+  componentDidMount() {
+    this.sortName()
+    console.log("called")
+  }
+
+
+  sortName = () => {
+    var items = [{
+        name: 'Hira',
+     
+      },
+      {
+        name: 'James',
+        
+      }
+     
+    ];
+
+   
+
+    // sort by name
+    items.sort(function (a, b) {
+      var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+
+      // names must be equal
+      return 0;
+    });
+    console.log(items)
+  };
+
   removeFriend = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
     const friends = this.state.friends.filter(friend => friend.id !== id);
     // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+    this.setState({
+      friends
+    });
   };
+
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
+
     return (
       <Wrapper>
-        <Title>Employee List</Title>
-        <header id ="h1">Image         Name        Occupation        Location </header>
+        <Title>Friends List</Title>
+        <header id="h1"> Image Name Occupation Location</header>
         {this.state.friends.map(friend => (
           <FriendCard
             removeFriend={this.removeFriend}
@@ -38,5 +81,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./style.css";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function FriendCard(props) {
   return (
    
 
-    <div className="row"  >
-
+    <div className="row" id="header" >
+  
+       <div className="col-sm-2 col-md-2 col-lg-2" id="img-container" >
+          <img alt={props.name} src={props.image} />
+           </div>
 
     <div className="col-sm-3 col-md-3 col-lg-3" id ="content">
            {props.name}
@@ -17,11 +20,8 @@ function FriendCard(props) {
             </div>
             <div className="col-sm-2 col-md-2 col-lg-3" id ="content2">
           {props.location}
-            
-    </div>
-       <div className="col-sm-2 col-md-2 col-lg-2" id="img-container" >
-          <img alt={props.name} src={props.image} />
-           </div>
+            </div>
+  
     
        
       <span onClick={() => props.removeFriend(props.id)} className="remove">
